b14a7c6b995600db3fbc174c803ed5a6e646f5eb
Rodney van den Velden <rodneymichael2002@gmail.com>
2023-03-16 16:02:11 +0100

    change admin

--- src/index.ts
+++ src/index.ts
@@ -55,7 +55,7 @@ client.on('room.message', async (roomId: string, ev: any) => {
 
   if (!command) return;
   if (command.admin) {
-    if (event.sender === await client.getUserId()) command.run(roomId, event, args, client);
+    if (event.sender === '@rodney:iusearchbtw.nl') command.run(roomId, event, args, client);
   } else {
     command.run(roomId, event, args, client);
   }
